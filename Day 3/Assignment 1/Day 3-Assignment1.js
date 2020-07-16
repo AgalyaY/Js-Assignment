5f340d507cba5fd98fa44f69375c92f8eeed6f1e
AgalyaY <63297205+AgalyaY@users.noreply.github.com>
2020-07-16 19:29:25 +0530

    Add files via upload

---
+++ Day 3/Assignment 1/Day 3-Assignment1.js
@@ -0,0 +1,14 @@
+let number = prompt("Enter a number:)",0);
+function check(number){
+    if(number%2==0){
+        result = "Even";
+        return result;
+    }
+    else{
+        result = "Odd";
+        return result;
+    }
+}
+
+let resultMsg = check(number);
+console.log(`The number entered is ${number} and the number is ${result}.`);
