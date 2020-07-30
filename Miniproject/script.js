68dc69a3c12db4154cbc6382e63484096704e41d
AgalyaY <63297205+AgalyaY@users.noreply.github.com>
2020-07-30 18:57:19 +0530

    Update script.js

--- Miniproject/script.js
+++ Miniproject/script.js
@@ -1,86 +1,3 @@
-/*var t = prompt("Enter a time in the below format:)","00:00:02:849");
-var	clsStopwatch = function() {
-    // Private vars
-    var	startAt	= 0;	// Time of last start / resume. (0 if not running)
-    var	lapTime	= 0;	// Time on the clock when last stopped in milliseconds
-
-    var	now	= function() {
-            return (new Date()).getTime(); 
-        }; 
-
-    // Public methods
-    // Start or resume
-    this.start = function() {
-            startAt	= startAt ? startAt : now();
-        };
-
-    // Stop or pause
-    this.stop = function() {
-            // If running, update elapsed time otherwise keep it
-            lapTime	= startAt ? lapTime + now() - startAt : lapTime;
-            startAt	= 0; // Paused
-        };
-
-    // Reset
-    this.reset = function() {
-            lapTime = startAt = 0;
-        };
-
-    // Duration
-    this.time = function() {
-            return lapTime + (startAt ? now() - startAt : 0); 
-        };
-};
-
-var x = new clsStopwatch();
-var $time;
-var clocktimer;
-
-function pad(num, size) {
-var s = "0000" + num;
-return s.substr(s.length - size);
-}
-
-function formatTime(time) {
-var h = m = s = ms = 0;
-var newTime = '';
-
-h = Math.floor( time / (60 * 60 * 1000) );
-time = time % (60 * 60 * 1000);
-m = Math.floor( time / (60 * 1000) );
-time = time % (60 * 1000);
-s = Math.floor( time / 1000 );
-ms = time % 1000;
-
-newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
-return newTime;
-}
-
-function show() {
-$time = document.getElementById('time');
-update();
-}
-
-function update() {
-$time.innerHTML = formatTime(x.time());
-}
-
-function start() {
-clocktimer = setInterval("update()", 1);
-x.start();
-}
-
-function stop() {
-x.stop();
-clearInterval(clocktimer);
-}
-
-function reset() {
-stop();
-x.reset();
-update();
-}*/
-
 
 //Make sure DOM is ready
 document.addEventListener("DOMContentLoaded",function(){
@@ -102,12 +19,8 @@ document.addEventListener("DOMContentLoaded",function(){
                 ["To create, one must first question everything", "Eileen Gray"],
                 ["The more you love the least deserving on your list, the more your life will change.", "Mike Dooley"],
                 ["You’ve got to bumble forward into the unknown", "Frank Gehry"]];
-
-// VARIABLES
   let oldQuoteIndex;
-  let oldColorIndex;
-
-//Generate a random number based on argument's length
+ //Generate a random number based on argument's length
   function generateNumber(dataPool) {
     return Math.floor(Math.random() * dataPool.length);
   }
@@ -145,9 +58,6 @@ document.addEventListener("DOMContentLoaded",function(){
     let text = document.querySelector(".container");
     let border = document.querySelector(".border");
     let randomQuoteBorder = document.querySelectorAll(".border-smaller")[0];
-
-//update index checker
-    oldColorIndex = index;
   }
 
 //Show new quote and change color scheme on 'Random Quote' button click
